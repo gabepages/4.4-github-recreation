18ff2a276040f922e185fe24aa791135099ab536
Gabriel Pages <gbpages@yahoo.com>
2016-02-25 19:01:27 -0500

    github page

---
+++ app/scripts/githubtoken.js
@@ -0,0 +1,1 @@
+module.exports = {'token': 'ebbf05b15d5f34fea62bc6185eb78a5e1730f508'};
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,66 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var handlebars = require('handlebars');
+var githubtoken = require('./githubtoken.js').token;
+
+if(typeof(githubtoken) !== "undfined"){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + githubtoken,
+    }
+  });
+}
+
+var myUser = 'gabepages';
+var userUrl = 'https://api.github.com/users/' + myUser;
+var userRepoUrl = 'https://api.github.com/users/' + myUser + '/repos';
+
+$.getJSON(userUrl,function(data){
+  console.log(data);
+
+  data.created_at = datePicker(data);
+
+//***********************
+//filling template
+//***********************
+  var source = $('#side-bar-content').html();
+  var templateSource = handlebars.compile(source);
+  var compiled = templateSource(data);
+  $('.side-col').html(compiled);
+
+  var source = $('#profile-pic').html();
+  var templateSource = handlebars.compile(source);
+  var compiled = templateSource(data);
+  $('.profile-pic').html(compiled);
+
+
+
+});
+
+$.getJSON(userRepoUrl,function(data){
+
+  data = orderedData(data);
+  var source = $('#repo').html();
+  var templateSource = handlebars.compile(source);
+  var compiled = templateSource({'repo': data});
+  $('.repo').html(compiled);
+});
+
+
+function orderedData(data){
+ data =  _.sortBy(data, function(data){
+    return data.pushed_at;
+  });
+data = data.reverse();
+return data;
+}
+
+function datePicker(data){
+  var date =new Date ($(data.created_at).selector);
+  var shortMonth =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov','Dec']
+  var month = shortMonth[date.getMonth()];
+  var day = date.getDate();
+  var year = date.getYear() + 1900;
+  var newDate = month + " " + day + " " + year;
+  return newDate;
+}
